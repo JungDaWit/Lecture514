97d413f23a9553aca3aec329c58245ad7ef401f5
JungDaWit <ekdnlt10@gmail.com>
2025-05-14 11:10:38 +0900

    [set] 각 패턴들 네임스페이스 추가

--- Assets/Scripts/DesionPatterns/ObservableProperty.cs
+++ Assets/Scripts/DesionPatterns/ObservableProperty.cs
@@ -3,39 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-public class ObservableProperty<T>
+namespace DesignPatten
 {
-    [SerializeField] private T _value;
-
-    public T Value
+    public class ObservableProperty<T>
     {
-        get => _value;
-        set
+        [SerializeField] private T _value;
+
+        public T Value
         {
-            if (_value.Equals(value)) return;
-            _value = value;
+            get => _value;
+            set
+            {
+                if (_value.Equals(value)) return;
+                _value = value;
+            }
         }
-    }
-    private UnityEvent<T> onValueChanged = new();
+        private UnityEvent<T> onValueChanged = new();
 
-  
+        public ObservableProperty(T value = default)
+        {
 
-    public void Subscrie(UnityAction<T> action)
-    {
-        onValueChanged.AddListener(action);
+        }
+
+        public void Subscrie(UnityAction<T> action)
+        {
+            onValueChanged.AddListener(action);
+        }
+        public void Unsubscride(UnityAction<T> action)
+        {
+            onValueChanged.RemoveListener(action);
+        }
+        public void UnsubscrideAll()
+        {
+            onValueChanged.RemoveAllListeners();
+        }
+        private void Nitify()
+        {
+            onValueChanged?.Invoke(Value);
+        }
+
     }
-    public void Unsubscride(UnityAction<T> action)
-    {
-        onValueChanged.RemoveListener(action);
-    }
-    public void UnsubscrideAll()
-    {
-        onValueChanged.RemoveAllListeners();
-    }
-    private void Nitify()
-    {
-        onValueChanged?.Invoke(Value);
-    }
-    
 }
