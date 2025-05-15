ae54d7ede22eaab7a676be4155a0cb30d5993afc
JungDaWit <ekdnlt10@gmail.com>
2025-05-15 09:44:17 +0900

    add 에셋 추가

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DesignPatten;
+using DesignPattern;
 
 public class PlayerStatus : MonoBehaviour
 {
-    [field : SerializeField][field: Range(0, 10)]
-
+    [field: SerializeField][field: Range(0, 10)] 
     public float WalkSpeed { get; set; }
 
     [field: SerializeField][field: Range(0, 10)]
